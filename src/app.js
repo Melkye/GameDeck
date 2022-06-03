315aef6d7b0158e0a182bed9d5613896bda6c72e
Denys Adamov <melkye777@gmail.com>
2022-06-03 14:39:38 +0300

    modified app.js to work with users

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+
 const gameRoutes = require("../routes/Game.Route");
+const userRoutes = require("../routes/User.Route");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 
 app.use("/games", gameRoutes);
+app.use("/users", userRoutes);
 
 module.exports = app;

416093550b5d0d14ab018e79aea60ba34411c290
Denys Adamov <melkye777@gmail.com>
2022-06-15 21:28:05 +0300

    added db connection with mongoose and inserted dotenv into root

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const gameRouter = require("../routers/Game.Router");
 const userRouter = require("../routers/User.Router");
+const articleRouter = require("../routers/Article.Router");
 
 const app = express();
+
+const url = "mongodb://0.0.0.0:27017/Gamedeck";
+
+mongoose
+    .connect(url)
+    .then(() => { console.log("Connected correctly to server");})
+    .catch((err) => { console.log(err); });
 
 app.use(express.json());
 
 app.use("/games", gameRouter);
 app.use("/users", userRouter);
+app.use("/articles", articleRouter);
 
 module.exports = app;
